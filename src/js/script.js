04c9ef4acf376233edde0f7f967a2587de8e6598
Oleg-beeps <117391606+OlegWajtulewicz@users.noreply.github.com>
2023-02-09 15:19:55 +0100

    tiny-slider

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,17 @@ $(document).ready(function(){
         speed: 700,
         //adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-left-solid.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-right-solid.png"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-right-solid.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                  dots: true,
+                  arrows: false
+                }
+              }
+        ]
+        
       });
   });
       
