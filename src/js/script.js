e542d7cd6bfaecf3fea391ad4bfc3c6eb404b483
Oleg-beeps <117391606+OlegWajtulewicz@users.noreply.github.com>
2023-02-09 12:23:50 +0100

    'icons'

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 700,
-        adaptiveHeight: true
+        //adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-left-solid.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-right-solid.png"></button>'
       });
   });
       
