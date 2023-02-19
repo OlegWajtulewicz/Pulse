04a9a0490c97f097599ec3b82e82ac89c8dadac7
Oleg-beeps <117391606+OlegWajtulewicz@users.noreply.github.com>
2023-02-20 00:21:11 +0100

    carouselDots

--- src/js/script.js
+++ src/js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
             {
                 breakpoint: 992,
                 settings: {
-                  dots: false,
+                  dots: true,
                   arrows: false
                 }
             }
